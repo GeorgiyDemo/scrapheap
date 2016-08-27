8036fecaf120c4513ead46b92e2d4f588c990a45
Georgiy Demenchuk <demenchuk.george@mail.ru>
2016-08-27 17:11:22 +0300

    Instagram promote

---
+++ InstaFollowCont.py
@@ -0,0 +1,13 @@
+import requests,json,time
+#Можно редачить
+shashlik = input('Вставь хеш\n>> ')
+while True:
+	r1 = requests.get('http://www.morelikes.me/service/view/?hash='+shashlik)
+	middle = (json.loads(r1.text))['usertofollow-id']
+	print(middle)
+	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+	payload = 'hash='+shashlik+'&user='+middle+'&answer=%7B%22status%22%3A%20%22ok%22%2C%20%22friendship_status%22%3A%20%7B%22incoming_request%22%3A%20false%2C%20%22followed_by%22%3A%20false%2C%20%22outgoing_request%22%3A%20false%2C%20%22following%22%3A%20true%2C%20%22blocking%22%3A%20false%2C%20%22is_private%22%3A%20false%7D%7D'
+	r = requests.post('http://www.morelikes.me/service/follow/', data=payload, headers=headers)
+	print('Status:'+json.loads(r.text)['result']+'\n'+json.loads(r.text)['message'])
+	gold = json.loads(r.text)['customer']['coins']
+	print('Кол-во балласов: '+str(gold))
---
+++ InstaLike.py
@@ -0,0 +1,12 @@
+import requests,json,time
+shashlik = input('Вставь хеш\n>> ')
+i = 0
+while True:
+	r1 = requests.get('http://www.multiliker.com/service/view/?hash='+shashlik)
+	middle = (json.loads(r1.text))['picture']
+	print(middle)
+	headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+	payload = 'hash='+shashlik+'&media='+middle+'&answer=%7B%22status%22%3A%20%22ok%22%7D'
+	r = requests.post('http://www.multiliker.com/service/like2/', data=payload, headers=headers)
+	print('Status:'+json.loads(r.text)['result']+'\n'+json.loads(r.text)['message'])
+	print('Кол-во балласов: '+str(json.loads(r.text)['customer']['coins']))
