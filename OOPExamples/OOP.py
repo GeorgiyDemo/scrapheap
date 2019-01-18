65696651c6e15f52b7554308f5624485664d42d1
Georgiy Demenchuk <demenchuk.george@mail.ru>
2019-01-19 02:51:19 +0300

    Мемные OOP приколы

---
+++ OOPExamples/OOP.py
@@ -0,0 +1,34 @@
+#Основа
+class Greater:
+    def Great(self,input_str):
+        print(input_str)
+
+#Задание параметра через конструктор
+class BetterGreater:
+    #Это типо конструктор
+    def __init__(self, str):
+        self.str = str
+    def Great(self, newstr):
+        print(self.str+" "+newstr) 
+
+#Задание параметра через обычную функцию
+class MoreBetterGreater:
+    def setatr(self,atrib):
+        self.argstring = atrib
+    #Атрибут класса
+    CHECK = "123"
+    def getatr(self):
+        print(self.CHECK+" "+self.argstring)
+def main():
+    x = Greater()
+    x.Great("meow")
+
+    y = BetterGreater("KOTTT")
+    y.Great("MOEWWW")
+
+    z = MoreBetterGreater()
+    z.setatr("ТАРАКАН ГЕННАДИЙ")
+    z.getatr()
+
+if __name__ == "__main__":
+    main()
---
+++ OOPExamples/memes.py
@@ -0,0 +1,29 @@
+class Memes:
+
+    X = int
+    def __init__(self,number):
+        self.number = number
+    def get_number(self):
+        return self.X(self.number)
+
+
+class SHARAGA:
+    def __init__(self):
+        self.mas = [ [1,] * 3 ]
+        print(self.mas)
+    def __check__(self):
+        self.mas[0][0] = "KOT"
+        print(self.mas)
+
+def main():
+    
+    listexample = {}
+    obj = Memes(3)
+    listexample[Memes] = str(obj.get_number())
+    print(listexample)
+
+    zaya = SHARAGA()
+    zaya.__check__()
+
+if __name__ == "__main__":
+    main()
