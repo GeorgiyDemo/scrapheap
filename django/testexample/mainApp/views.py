3393e64db5f8e1bc1c62ce7661c1eb58974a0870
Georgiy Demenchuk <demenchuk.george@mail.ru>
2019-07-01 00:25:13 +0300

    Смотрим на стандартный шаблонизатор

--- django/testexample/mainApp/views.py
+++ django/testexample/mainApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Окей, веселье пошло")
+    return render(request, "mainApp/homepage.html")
--- django/testexample/testexample/wsgi.py
+++ django/testexample/testexample/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for avitodumper project.
+WSGI config for testexample project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avitodumper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testexample.settings')
 
 application = get_wsgi_application()
