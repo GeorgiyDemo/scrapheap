88963770fa215d55f4cbc3035fbfe5a9114838b4
Georgiy Demo <demenchuk.george@protonmail.com>
2020-02-14 11:27:45 +0300

    Актуальные примеры

---
+++ OOP_example.py
@@ -0,0 +1,24 @@
+class Rectangle:
+    def __init__(self, width):
+        self.__width = width
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        if w > 0:
+            self.__width = w
+        else:
+            raise ValueError
+
+#Теперь работать с width и height можно так, как будто они являются атрибутами:
+rect = Rectangle(10)
+print(rect.width)
+
+#Можно не только читать, но и задавать новые значения свойствам: rect.width = 50
+rect.height = 70
+print(rect.width)
+print(rect.height)
+#Если вы обратили внимание: в setter’ах этих свойств осуществляется
+#проверка входных значений, если значение меньше нуля, то будет выброшено исключение ValueE rect.width = -50
---
+++ mp_requests.py
@@ -0,0 +1,23 @@
+import multiprocessing as mp
+import requests
+import time
+def foo(q):
+    while True:
+        r = requests.get("https://google.ru")
+        print("proc2", r.status_code)
+        time.sleep(1)
+
+def check():
+    while True:
+        r = requests.get("https://ya.ru")
+        print("proc1", r.status_code)
+        time.sleep(1)
+
+    
+if __name__ == '__main__':
+
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=foo, args=(q,))
+    p.start()
+    check()
