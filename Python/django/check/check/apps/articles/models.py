b36db433967f40310432993d20776cee85f4b623
Demka <GeorgiyDemo@users.noreply.github.com>
2021-07-01 01:14:46 +0300

    Добавил базовые модели

--- Python/django/check/check/apps/articles/models.py
+++ Python/django/check/check/apps/articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField("Название статьи", max_length=200)
+    text = models.TextField("Текст статьи")
+    pub_date = models.DateTimeField("Дата публикации")
+
+    def __str__(self):
+        return f"title: {self.title}, text: {self.text}, pub_date: {self.pub_date}"
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    name = models.CharField("Имя автора", max_length=50)
+    text = models.CharField("Текст комментария", max_length=200)
+
+    def __str__(self):
+        return f"article: {self.article}, name: {self.name}, text:{self.text}"
--- Python/django/check/check/settings.py
+++ Python/django/check/check/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -27,7 +30,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles.apps.ArticlesConfig'
 ]
 
 MIDDLEWARE = [
@@ -70,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'check.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +82,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -113,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
