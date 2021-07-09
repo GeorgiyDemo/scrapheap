8ab032a9c7372aa91a5cd60b230eaab42c06bc10
Demka <GeorgiyDemo@users.noreply.github.com>
2021-07-09 16:15:55 +0300

    Какие-то примеры джунги

--- Python/django/check/check/apps/articles/views.py
+++ Python/django/check/check/apps/articles/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world")
+
+
 # Create your views here.
 def test(request):
     return HttpResponse("TEST")
+
+
+class OrderView():
+    pass
