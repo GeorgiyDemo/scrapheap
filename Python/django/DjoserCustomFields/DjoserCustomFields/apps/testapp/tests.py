d9d27bcb9ecadcaaa38a78d8a9019e968cffee16
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2021-08-01 15:54:29 +0300

    Все работает

--- Python/django/DjoserCustomFields/DjoserCustomFields/apps/testapp/tests.py
+++ Python/django/DjoserCustomFields/DjoserCustomFields/apps/testapp/tests.py
@@ -1,1 +1,8 @@
-# Create your tests here.
+
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+class RouterAPITestCase(APITestCase):
+
+    def test_get(self):
+        self.assertTrue(True)
